de69828e1785236ec57c70c3007a04bd7a54248f
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-27 02:15:35 -0400

    finish assignment

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -9,9 +9,36 @@ function playerOneWins(p1, p2) {
   return test;
 }
 
-function getHand(){
-  var weapons = ["rock", "paper", "scissors"];
-  return weapons[sample(3)];
+// function getHand(){
+//   var weapons = ["rock", "paper", "scissors"];
+//   return weapons[sample(3)];
+// }
+
+function playRound(p1, p2){
+  var playerOneWeapon = p1.getHand();
+  var playerTwoWeapon = p2.getHand();
+  // console.log(p1.name + " has chosen " + playerOneWeapon);
+  // console.log(p2.name + " has chosen " + playerTwoWeapon);
+  if (playerOneWeapon === playerTwoWeapon) {
+    console.log("the game is a tie");
+    return null;
+  } else if (playerOneWins(playerOneWeapon, playerTwoWeapon)) {
+    p1.score++;
+    console.log(p1.name + " wins this round");
+    console.log(p1.name + "'s score is " + p1.score);
+    return p1;
+  } else {
+    p2.score++;
+    console.log(p2.name + " wins this round");
+    console.log(p2.name + "'s score is " + p2.score);
+    return p2;
+  }
+}
+
+function playGame(player1, player2, playUntil){
+  while (player1.score < playUntil && player2.score < playUntil) {
+    playRound(player1, player2)
+  }
 }
 
 var playerOne = {
@@ -30,26 +57,6 @@ var playerTwo = {
     return weapons[sample(3)];
   },
   score: 0
-}
-
-function playRound(p1, p2){
-  var playerOneWeapon = p1.getHand();
-  var playerTwoWeapon = p2.getHand();
-  if (playerOneWeapon === playerTwoWeapon) {
-    return null;
-  } else if (playerOneWins(playerOneWeapon, playerTwoWeapon)) {
-    p1.score++;
-    return p1;
-  } else {
-    p2.score++;
-    return p2;
-  }
-}
-
-function playGame(player1, player2, playUntil){
-  while (player1.score < playUntil && player2.score < playUntil) {
-    playRound(player1, player2)
-  }
 }
 
 playGame(playerOne,playerTwo,5);
